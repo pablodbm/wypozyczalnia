bb6bea8adebed1389739f335867161baadaa7bda
pablodbm <pawel.slota21@gmail.com>
2022-04-07 17:58:03 +0200

    add employee

--- backend/add_employee.php
+++ backend/add_employee.php
@@ -3,5 +3,6 @@ require "./db_connect.php";
 $login = $_POST["login"];
 $password = $_POST["password"];
 $fullName = $_POST["fullName"];
-$add = "INSERT INTO users (fullName,login,password) VALUES ('$fullName','$login','$password')";
+$add = "INSERT INTO users (fullName,login,password,user_type) VALUES ('$fullName','$login','$password',2)";
 $mysqli->query($add);
+header("Location:../adminPanel.php");
