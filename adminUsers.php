7e27ea334588938f7e949a69de4698c3c655c2c5
kacperPrzelozny <przelozny14@gmail.com>
2022-04-08 12:14:10 +0200

    ssss

--- adminUsers.php
+++ adminUsers.php
@@ -5,13 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
     <title>Admin page - Zarządzanie Pracownikami</title>
     <link rel="stylesheet" href="style.css">
 
-=======
-    <title>Document</title>
->>>>>>> e794938c0bf26823695ce1d09d05952c081a1bc5
 </head>
 
 <body>
@@ -20,7 +16,6 @@
         header("Location:./index.php");
     }
     ?>
-<<<<<<< HEAD
     <header>
         <div class="logo">
             <h1>Panel administratora</h1>
@@ -118,46 +113,6 @@
         </div>
     </div>
     
-=======
-
-    <div class="add_employee">
-        <?php if ($_SESSION["user_type"] == 2) : ?>
-            <h1>Dodanie pracownika</h1>
-            <form action="./backend/add_employee.php" method="post">
-                <input type="text" name="fullName" id="">
-                <input type="text" name="login" id="">
-                <input type="password" name="password" id="">
-                <input type="submit" value="dodaj pracownika">
-            </form>
-        <?php endif ?>
-    </div>
-    <div class="delete_empoyee">
-        <form action="" method="post">
-            <select name="single_employee" id="">
-                <?php
-                require "./backend/db_connect.php";
-                $select_workers = "SELECT * FROM users WHERE user_type=1";
-                $result = $mysqli->query($select_workers);
-                $make_options = false;
-                if ($result->num_rows == 0) {
-                    $result = $result->fetch_assoc();
-                    $make_options = true;
-                } else if ($result->num_rows > 0) {
-                    $result->fetch_all(MYSQLI_ASSOC);
-                    $make_options = true;
-                } else {
-                    echo "<div class='brak_pracownikow'>Brak pracowników</div>";
-                }
-                if ($make_options == true) {
-                    foreach ($result as $single) {
-                        echo "<option value='" . $single["id"] . "'>" . $single["fullName"] . "</option>";
-                    }
-                }
-                ?> </select>
-            <input type="submit" value="Usuń pracownika" name="delete">
-        </form>
-    </div>
->>>>>>> e794938c0bf26823695ce1d09d05952c081a1bc5
 </body>
 
 </html>
